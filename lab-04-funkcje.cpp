6972ae77825b7a395aa7a25783b85d271e39aace
Marcin Ciura <mciura@gmail.com>
2026-03-23 18:51:38 +0100

    Laboratorium 4

---
+++ lab-04-funkcje.cpp
@@ -0,0 +1,78 @@
+#include <chrono>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <vector>
+
+int main ()
+{
+	// funkcje
+
+	std::default_random_engine silnik; // silnik do generowania liczb losowych
+	silnik.seed(std::chrono::system_clock::now().time_since_epoch().count()); // ustawienie ziarna silnika z użyciem aktualnego czasu
+
+	std::normal_distribution<double> rozklad {50, 20};
+
+// 	////////////////////////////////////////////
+// 	// 0. rozgrzewka
+// 	czolem();  // Funkcja pisze cos milego na ekranie
+// 	
+// 	////////////////////////////////////////////
+// 	// 1. 
+// 	double a = rozklad(silnik);
+// 	double b = rozklad(silnik);
+// 	// Funkcja maksimum zwraca wartosc wieksza z dwoch:
+// 	double wieksza = maksimum(a, b);
+// 	std::cout << "maksimum(" << a << ", " << b << ") == " << wieksza << std::endl;
+// 	
+// 	////////////////////////////////////////////
+// 	// 2. 
+// 	double c = rozklad(silnik);
+// 	// Funkcja srodkowa zwraca wartosc srodkowa pod wzgledem wartosci.
+// 	double wartosc_srodkowa = srodkowa(a, b, c);
+// 	std::cout << "srodkowa(" << a << ", " << b << ", " << c << ") == " << wartosc_srodkowa << std::endl;
+// 	
+// 	////////////////////////////////////////////
+// 	// 3.
+//     	
+// 	const int N { 10 };
+//	const double SREDNIA { 170.0 };
+//	const double ODCH_STD { 10.0 };
+// 	std::vector<double> wektor;
+// 	
+// 	// Funkcja wypełnia wektor N wartościami pseudolosowymi
+//	// o rozkładzie normalnym ze średnią SREDNIA i odchyleniem standardowym ODCH_STD.
+// 	wypelnij(silnik, SREDNIA, ODCH_STD, N, &wektor);
+// 	
+// 	// Funkcja wypisuje wektor na ekran (użyć const std::vector<double>&)
+// 	wypisz(wektor);
+// 	
+// 	////////////////////////////////////////////
+// 	// 4. 
+// 	// Funkcja zwraca wartosc srednia z wektora.
+// 	auto srednia_z_wektora = srednia(wektor);
+// 	
+// 	std::cout << "srednia wektora: " << srednia_z_wektora << std::endl;
+// 	
+// 	////////////////////////////////////////////
+// 	// 5. 
+// 	// Funkcja zwraca odchylenie standardowe z wektora.
+// 	auto odchylenie_z_wektora = odchylenie(wektor);
+// 	
+// 	std::cout << "odchylenie wektora: " << odchylenie_z_wektora << std::endl;
+// 	
+// 	////////////////////////////////////////////
+// 	// 6. 
+// 	wypisz(wektor);
+// 	// Funkcja przesuwa wartosci wektora o jedna komorke w prawo.
+//	// Ostatnia wartosc trafia do komorki zerowej.
+// 	rotacja_w_prawo(&wektor);
+// 	wypisz(wektor);
+// 	
+// 	for (int i = 0; i < 2 * N; i++)
+// 	{
+// 		rotacja_w_prawo(&wektor);
+// 		wypisz(wektor);
+// 	}
+	return 0;	
+}
